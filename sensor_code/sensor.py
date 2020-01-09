b84e3e16fbf7a3e4ce3d3e4140122ba7dd05fdbd
liatmoss <liatmoss@hotmail.com>
2020-01-09 10:39:42 +0000

    Test passing Camera added

--- sensor_code/sensor.py
+++ sensor_code/sensor.py
@@ -1,13 +1,19 @@
-def pir():
-    from gpiozero import MotionSensor
-    pir = MotionSensor(4)
+from gpiozero import MotionSensor
+from picamera import PiCamera
+import requests
+from auth import (key)
+
+def pir(motion_sensor):
+    pir = motion_sensor(4)
     pir.wait_for_motion()
 
-def sensor():
-    import requests
-    from auth import (key)
-    pir
+def sensor(motion_sensor = MotionSensor):
+    camera = PiCamera()
+    pir(motion_sensor)
+    camera.capture()
+    camera.close
     r = requests.post(key)
-    print("Motion detected!")
+    return "Motion detected!"
 
-sensor()
+if __name__ == '__main__':
+    sensor()
--- sensor_code/test_sensor.py
+++ sensor_code/test_sensor.py
@@ -1,11 +1,19 @@
 import unittest
 from sensor import *
 
+class mockMotionSensor:
+    def __init__(number):
+        pass
+
+    def wait_for_motion(self):
+        sleep(1)
+
+
 class SensorTestCase(unittest.TestCase):
 
     def test_sensor_activated(self):
-        
-        self.assertEqual(1, "Motion detected!")
+
+        self.assertEqual(sensor(mockMotionSensor), "Motion detected!")
 
 
     if __name__ == '__main__':
