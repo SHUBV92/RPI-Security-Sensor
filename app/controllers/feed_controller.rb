35b4d409eb8cf938071d6044e35c4a0cd53e709f
Topher <topherb99@gmail.com>
2020-01-10 15:54:24 +0000

    added: post request to index

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,11 +1,17 @@
 require 'base64'
 
 class FeedController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  # def create
+  #   @image = Base64.decode64(params[:image])
+  #   redirect_to controller: 'feed', action: 'images'
+  #   File.open('./public/image.png', 'wb+') do |f|
+  #     f.write(Base64.decode64(params[:image]))
+  #   end
+  # end
+
   def images
-    if params[:image] == nil
-      @image = "Not Found"
-    else
-      @image = Base64.decode64(params[:image])
-    end
+    @image = Base64.decode64(params[:image])
+    render 'feed/images'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'feed/images/:image', to: 'feed#images'
+  get 'feed/images', to: 'feed#images'
 
-  root to: "application#index"
+  post "/", to: 'feed#images'
+
+  root to: "feed#images"
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
