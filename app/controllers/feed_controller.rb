b1245f39d2e8b647bb6c4df7257a94c6fcf4a3c3
Topher <topherb99@gmail.com>
2020-01-10 12:03:13 +0000

    added route and params

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,4 +1,11 @@
+require 'base64'
+
 class FeedController < ApplicationController
   def images
+    if params[:image] == nil
+      @image = "Not Found"
+    else
+      @image = Base64.decode64(params[:image])
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'feed/images'
+  get 'feed/images/:image', to: 'feed#images'
 
   root to: "application#index"
   devise_for :users
