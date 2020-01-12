2b3d939111aa9b969c2abfb946abfcb7392fa4bb
liatmoss <liatmoss@hotmail.com>
2020-01-12 18:13:36 +0000

    Need to log-in before seeing security images

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -3,6 +3,8 @@ require 'date'
 
 class FeedController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
+
   # def create
   #   @image = Base64.decode64(params[:image])
   #   redirect_to controller: 'feed', action: 'images'
