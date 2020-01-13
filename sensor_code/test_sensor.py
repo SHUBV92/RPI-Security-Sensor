df2ffa5db3dd5d6375a5a5042be45f02bf921120
mattfreeman-london <mathew.s.l.freeman@gmail.com>
2020-01-13 15:00:57 +0000

    update: tidied test_sensor.py for style

--- sensor_code/test_sensor.py
+++ sensor_code/test_sensor.py
@@ -8,13 +8,9 @@ class mockMotionSensor:
     def wait_for_motion(self):
         sleep(1)
 
-
 class SensorTestCase(unittest.TestCase):
-
     def test_sensor_activated(self):
-
         self.assertEqual(sensor(mockMotionSensor), "Motion detected!")
 
-
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+    unittest.main()
