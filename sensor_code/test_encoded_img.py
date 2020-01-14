7737ddf3aa8404773143b6400004f743330c7648
Student <student@Makerss-MacBook-Pro.local>
2020-01-14 14:48:45 +0000

    test added for image format

--- sensor_code/test_encoded_img.py
+++ sensor_code/test_encoded_img.py
@@ -4,7 +4,7 @@ from encoded_img import *
 class TestCase(unittest.TestCase):
 
     def test_encoded_img(self):
-        self.assertEqual(image, "Test")
+        self.assertIsInstance(image_manip(), str)
 
-    if __name__=='__main__':
-        unittest.main()
+if __name__=='__main__':
+    unittest.main()
