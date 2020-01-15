645977bf25cdbc81a5534e69c7d4022a1ddbd780
Student <student@Makerss-MacBook-Pro.local>
2020-01-15 16:18:50 +0000

    all tests passing

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
+  gem 'launchy'
   gem 'selenium-webdriver'
   gem 'travis'
   gem 'rspec-rails', '~> 3.5'
@@ -54,4 +55,3 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
-
--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
-# RSpec.feature "Sign in", type: :feature do
-#   scenario "Can sign in" do
-#     visit "/"
-#     # click_link "Sign up"
-#     fill_in "Email", with: "test@test.com"
-#     fill_in "Password", with: "test_pass"
-#     fill_in "Password confirmation", with: "test_pass"
-#     click_button "Sign up"
-#     click_link "Sign out"
-#     click_link "Sign in"
-#     fill_in "Email", with: "test@test.com"
-#     fill_in "Password", with: "test_pass"
-#     click_button "Sign in"
-#     expect(page).to have_content("Signed in successfully.")
-#   end
-#
-# end
+RSpec.feature "Sign in", type: :feature do
+  scenario "Can sign in" do
+    visit "/"
+    click_link "Sign up"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "test_pass"
+    fill_in "Password confirmation", with: "test_pass"
+    click_button "Sign up"
+    click_link "Sign out"
+    click_link "Sign in"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "test_pass"
+    click_button "Sign in"
+    expect(page).to have_content("Signed in successfully.")
+  end
+
+end
