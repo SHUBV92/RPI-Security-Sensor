e64a49ba84c16574e42f83a4de0d8be6cece886d
Topher <topherb99@gmail.com>
2020-01-16 14:33:37 +0000

    added: SHOW test for feed controller

--- spec/controllers/feed_controller_spec.rb
+++ spec/controllers/feed_controller_spec.rb
@@ -1,12 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe FeedController, type: :controller do
+  include Devise::Test::ControllerHelpers
+
+  # let!(:user_image) { create(:feed, pi_key: "xxxxx") }
+
+  let(:user) { create(:user) }
+
+  before(:all) do
+    FactoryBot.define do
+      factory :user do
+        email { "test@2.com" }
+        password { "password" }
+      end
+    end
+  end
+
+  # before(:all) do
+  #   FactoryBot.define do
+  #     factory :feed do
+  #        { 'hello world'}
+  #     end
+  #   end
+  # end
+
+  before(:each) do
+    sign_in user
+  end
+
 
   describe "GET #show" do
-    xit "returns http success" do
+    it "returns http success" do
       get :show
       expect(response).to have_http_status(:success)
     end
   end
+
+
 
 end
